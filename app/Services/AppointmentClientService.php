1cc1d34e9cd12b57cefe740f650b6c55dbba67b9
M-noor-88 <taknoor45@gmail.com>
2024-09-24 23:02:29 +0300

    Finish Appointment controller

--- app/Services/AppointmentClientService.php
+++ app/Services/AppointmentClientService.php
@@ -7,11 +7,14 @@ use App\Jobs\CreateRatingJob;
 use App\Models\Appointment;
 use App\Models\Notification;
 use App\Models\Rating;
+
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class AppointmentClientService
 {
@@ -21,6 +24,12 @@ class AppointmentClientService
     {
         $this->firebaseService = $firebaseService;
     }
+
+    /*
+     * --------------------
+     * Client functions
+     * --------------------
+     */
 
     public function createAppointment(CreateAppointmentRequest $request, int $provider_id): JsonResponse
     {
@@ -80,7 +89,7 @@ class AppointmentClientService
     }
 
 
-    public function finishAppointmentAndRate($appointment_id, $data)
+    public function finishAppointmentAndRate($appointment_id, $data) : JsonResponse
     {
         // Get the authenticated client
         $client = Auth::user();
@@ -98,7 +107,6 @@ class AppointmentClientService
 
         // Wrap in a transaction to ensure atomicity
         DB::beginTransaction();
-
         try {
             // Update the appointment status to "finished"
             $appointment->update(['status' => 'finished']);
@@ -128,6 +136,201 @@ class AppointmentClientService
             Log::error('Error finishing appointment: ' . $e->getMessage());
             return response()->json(['message' => 'An error occurred while finishing the appointment. Please try again later.', 'Error' => $e->getMessage()], 500);
         }
+    }
+
+
+    public function showAppointment($app_id) : JsonResponse
+    {
+        $client = Auth::user();
+
+        $cacheKey = sprintf('appointment.client.%d.appointment.%d', $client->id, $app_id);
+
+        $appointment = Cache::remember($cacheKey, 1000, function () use ($client, $app_id) {
+            return Appointment::where('id', $app_id)
+                ->where('client_id', $client->id)
+                ->first(); // Use first() if you expect a single appointment
+        });
+
+        return response()->json($appointment);
+    }
+
+
+    public function cancelAppointment(Request $request, $appointment_id): JsonResponse
+    {
+        $client = Auth::user();
+        if (!$client->hasRole('client')) {
+            return response()->json(['message' => "You don't have permissions | Just for Client"], 403);
+        }
+
+        $request->validate(['status' => 'required|in:canceled']);
+
+        $appointment = Appointment::where('id', $appointment_id)
+            ->where('client_id', $client->id)
+            ->whereIn('status', ['approved', 'pending'])
+            ->first();
+
+        if (!$appointment) {
+            return response()->json(['message' => 'Appointment not found or you do not have permission to update this appointment'], 404);
+        }
+
+        // Check if cancellation is allowed (24-hour rule)
+        if ($this->isCancellationAllowed($appointment->date)) {
+            return response()->json(['message' => 'Cannot cancel the appointment. Less than 24 hours remain before the appointment date.'], 400);
+        }
+
+        $appointment->update(['status' => 'canceled']);
+
+        return response()->json(['message' => 'Appointment canceled successfully']);
+    }
+
+    private function isCancellationAllowed(string $appointmentDate): bool
+    {
+        return Carbon::now()->diffInHours(Carbon::parse($appointmentDate), false) <= 24;
+    }
+
+
+    /*
+     * --------------------
+     * Provider functions
+     * --------------------
+     */
+
+    // get Requests , Pending | Accepted Appointments for provider
+    public function getRequestsAndAcceptedAppointment() : JsonResponse
+    {
+        $provider = Auth::user();
+        if(!$provider->hasRole('provider')) {
+            return response()->json(['message' => "Not Authorized | Providers Only"] , 403);
+        }
+
+        $cacheKey = 'requests'.$provider->id;
+
+        $appointments = Cache::remember($cacheKey , 1000 , function () use ($provider) {
+           return Appointment::where('provider_id', $provider->id)
+               ->whereIn('status', ['pending', 'approved'])
+               ->with('client:id,name')->with('client.city') // Assuming there is a relationship defined in the Appointment model
+               ->get()
+               ->map(function ($appointment) {
+                   return [
+                       'id' => $appointment->id,
+                       'client' => $appointment->client->name,
+                       'status' => $appointment->status,
+                       'date'=> $appointment->date,
+                       'hour'=> $appointment->hours,
+                       'city'=>$appointment->client->city->city_name ?? null ,
+                   ];
+               });
+        });
+
+        return response()->json(['appointments' => $appointments]);
+
+    }
+
+    public function getCompletedAppointment() : JsonResponse
+    {
+        $provider = Auth::user();
+        if(!$provider->hasRole('provider')) {
+            return response()->json(['Message' => "Not Authorized | Providers Only"] , 403);
+        }
+
+        $cacheKey = 'Completed'.$provider->id;
+
+        $appointments = Cache::remember($cacheKey , 1000 , function () use ($provider) {
+            return Appointment::where('provider_id', $provider->id)
+                ->where('status', 'finished')
+                ->with('client')
+                ->get()
+                ->map(function ($appointment) {
+                    return [
+                        'id' => $appointment->id,
+                        'client' => $appointment->client->name,
+                        'status' => $appointment->status,
+                        'date'=> $appointment->date,
+                        'hour'=> $appointment->hours,
+                        'city'=>$appointment->client->city->city_name,
+                        'description'=> $appointment->description
+                    ];
+                });
+        });
+
+        return response()->json(['Completed' => $appointments]);
+    }
+
+    public function getRejectedAppointment() : JsonResponse
+    {
+        $provider = Auth::user();
+        if(!$provider->hasRole('provider')) {
+            return response()->json(['Message' => "Not Authorized | Providers Only"] , 403);
+        }
+
+        $cacheKey = 'Rejected'.$provider->id;
+        $appointments = Cache::remember($cacheKey , 1000 , function () use ($provider) {
+            return  Appointment::where('provider_id', $provider->id)
+                ->where('status', 'rejected')
+                ->with('client')
+                ->get()
+                ->map(function ($appointment) {
+                    return [
+                        'id' => $appointment->id,
+                        'client' => $appointment->client->name,
+                        'status' => $appointment->status,
+                        'date'=> $appointment->date,
+                        'hour'=> $appointment->hours,
+                        'city'=>$appointment->client->city->city_name,
+                        'description'=> $appointment->description
+                    ];
+                });
+        });
+        return response()->json(['Rejected' => $appointments]);
+    }
+
+    public function updateAppointmentStatus(Request $request, $appointment_id): JsonResponse
+    {
+        $provider = Auth::user();
+
+        // Authorization check
+        if (!$provider->hasRole('provider')) {
+            return response()->json(['message' => "Not Authorized | Providers Only"], 403);
+        }
+
+        // Validate the input data
+        $validatedData = $request->validate([
+            'status' => 'required|in:approved,rejected',
+        ]);
+
+        // Find the appointment
+        $appointment = Appointment::where('id', $appointment_id)
+            ->where('provider_id', $provider->id)
+            ->with('client:id,name,device_token')
+            ->first();
+
+        if (!$appointment) {
+            return response()->json(['message' => "Appointment not found or you don't have permission to update this appointment"], 404);
+        }
+
+        // Update the appointment status
+        $appointment->update(['status' => $validatedData['status']]);
+
+        // Prepare and send notification
+        $this->sendAppointmentNotification($appointment, $provider);
+
+        return response()->json(['message' => 'Appointment status updated successfully', 'appointment' => $appointment], 200);
+    }
+
+    protected function sendAppointmentNotification(Appointment $appointment, $provider)
+    {
+        $title = "Appointment " . ucfirst($appointment->status);
+        $body = "Your appointment has been " . $appointment->status . " by " . $provider->name;
+
+        // Create notification record
+        Notification::create([
+            'user_id' => $appointment->client->id,
+            'title' => $title,
+            'details' => $body
+        ]);
+
+        // Send notification
+        $this->sendNotification($appointment->client->device_token, $title, $body);
     }
 
     protected function sendNotification($deviceToken, $title, $body)
